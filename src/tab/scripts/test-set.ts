8736695e5c71924de5c468181cf77f408889548a
lars <larsdood@gmail.com>
2021-11-15 19:13:18 +0900

    remove console logs

--- src/tab/scripts/test-set.ts
+++ src/tab/scripts/test-set.ts
@@ -3,12 +3,10 @@ import { getGoiQuestion } from './question-generators/goi/goi-question-generator
 import { getBunpouQuestion } from './question-generators/bunpou/bunpou-question-generator';
 
 export const generateTestSet = (level: 1|2|3|4|5) => {
-  console.log('testset from:', level)
   const kanjiQuestion = getKanjiQuestion(level);
   const goiQuestion = getGoiQuestion(level);
   const bunpouQuestion = getBunpouQuestion(level);
 
-  console.log(kanjiQuestion)
   return {
     kanji: kanjiQuestion,
     goi: goiQuestion,
